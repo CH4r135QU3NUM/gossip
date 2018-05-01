07b17d962274a1df8a803b2cf0ac68921ccf0b00
charles <charles.quenum@gmail.com>
2018-05-01 22:03:51 +0200

    migration

--- the-gossip-project/db/migrate/20180501200012_create_gossips.rb
+++ the-gossip-project/db/migrate/20180501200012_create_gossips.rb
@@ -1,7 +1,7 @@
 class CreateGossips < ActiveRecord::Migration[5.2]
   def change
     create_table :gossips do |t|
-      t.string, :anonymous_author
+      t.string :anonymous_author
       t.string :content
 
       t.timestamps
