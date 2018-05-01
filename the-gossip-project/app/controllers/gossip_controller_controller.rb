0e25b67fb7e92980e2fe8f70638c7bee48137dea
charles <charles.quenum@gmail.com>
2018-05-02 00:04:34 +0200

    version heroku

---
+++ the-gossip-project/app/controllers/gossip_controller_controller.rb
@@ -0,0 +1,19 @@
+class GossipControllerController < ApplicationController
+  def new
+@gossip = Gossip.new
+  end
+
+def create
+		@gossip = Gossip.new
+	 	@gossip.anonymous_author = params[:anonymous_author]
+	  	@gossip.content = params[:content]
+	  
+	 	@gossip.save
+	 	redirect_to (@gossip)
+	end
+
+def show
+    @gossip = Gossip.find(params[:id])
+ 	end
+
+end
